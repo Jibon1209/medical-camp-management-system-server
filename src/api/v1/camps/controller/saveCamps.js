451433245e34ddbbcf0f219aa1c8f250d91b98cb
Jibon1209 <jessore007@gmail.com>
2023-11-26 22:29:34 +0600

    added camps route

--- src/api/v1/camps/controller/saveCamps.js
+++ src/api/v1/camps/controller/saveCamps.js
@@ -1,8 +1,15 @@
+const mongoose = require("mongoose");
 const {
   findOrganizerIdByEmail,
 } = require("../../../../lib/findOrganizerIdByEmail");
 const Camps = require("../../../../models/Camps");
+
 const saveCamps = {
+  async getPopularCamps(req, res) {
+    const result = await Camps.find().sort({ $natural: -1 }).limit(8).exec();
+
+    res.send({ success: true, data: result });
+  },
   async getCampUserWise(req, res) {
     const user = req.params.email;
     const organizerId = await findOrganizerIdByEmail(user);
@@ -10,6 +17,13 @@ const saveCamps = {
       .populate("organizer", "email")
       .exec();
     res.send({ success: true, data: result });
+  },
+  async getCampIdWise(req, res) {
+    const id = req.params.id;
+    const result = await Camps.findOne({
+      _id: new mongoose.Types.ObjectId(id),
+    });
+    res.send(result);
   },
   async create(req, res) {
     try {
@@ -44,6 +58,46 @@ const saveCamps = {
       console.error(error);
       res.status(500).send({ error: "Internal Server Error" });
     }
+  },
+  async updateCampsIdWise(req, res) {
+    const {
+      campName,
+      location,
+      professional,
+      fees,
+      dateTime,
+      image,
+      services,
+      audience,
+      description,
+      organizerEmail,
+    } = req.body;
+    const organizerId = await findOrganizerIdByEmail(organizerEmail);
+    const id = req.params.id;
+    const filter = { _id: new mongoose.Types.ObjectId(id) };
+    const updatedDoc = {
+      $set: {
+        campName,
+        location,
+        professional,
+        fees,
+        dateTime,
+        image,
+        services,
+        audience,
+        description,
+        organizer: organizerId,
+      },
+    };
+
+    const result = await Camps.updateOne(filter, updatedDoc);
+    res.send({ success: true, data: result });
+  },
+  async campDeleteIdWise(req, res) {
+    const id = req.params.id;
+    const query = { _id: new mongoose.Types.ObjectId(id) };
+    const result = await Camps.deleteOne(query);
+    res.send({ success: true, data: result });
   },
 };
 module.exports = saveCamps;
--- src/routes/v1/camps/index.js
+++ src/routes/v1/camps/index.js
@@ -3,11 +3,25 @@ const verifyOrganizer = require("../../../middlewares/verifyOrganizer");
 const verifyToken = require("../../../middlewares/verifyToken");
 
 const router = require("express").Router();
+router.get("/popular/camps", saveCamps.getPopularCamps);
 router.get(
   "/all/camps/:email",
   verifyToken,
   verifyOrganizer,
   saveCamps.getCampUserWise
 );
+router.get("/camps/:id", saveCamps.getCampIdWise);
 router.post("/camps", verifyToken, verifyOrganizer, saveCamps.create);
+router.patch(
+  "/camps/:id",
+  verifyToken,
+  verifyOrganizer,
+  saveCamps.updateCampsIdWise
+);
+router.delete(
+  "/camps/:id",
+  verifyToken,
+  verifyOrganizer,
+  saveCamps.campDeleteIdWise
+);
 module.exports = router;
